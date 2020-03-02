01f915a72bfd0a2c61af901526fd904d442922ef
unknown <josephgroark@gmail.com>
2020-03-02 17:39:06 +0000

    Added logo to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,34 @@ import { css } from "@emotion/core"
 import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
+import logo from "../images/mikeyLogo.png"
 
 export default ({ data }) => {
   return (
     <Layout>
       <div>
+        <img
+          css={css`
+            float: right;
+          `}
+          src={logo}
+          alt="Mikey"
+        />
         <h1
           css={css`
-            display: inline-block;
-            border-bottom: 1px solid;
+            position: relative;
           `}
         >
           Fiction and fact from Mikeys almanack
         </h1>
-        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+
+        <h4
+          css={css`
+            display: inline-block;
+          `}
+        >
+          {data.allMarkdownRemark.totalCount} Posts
+        </h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
             <Link
