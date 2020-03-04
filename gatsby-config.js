bfeb00cdfd006080e5a27109be2676519250fdb8
unknown <josephgroark@gmail.com>
2020-03-04 13:00:15 +0000

    added guess.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,24 @@
+const dynamicPlugins = []
+// pick data from 3 months ago
+const startDate = new Date()
+startDate.setMonth(startDate.getMonth() - 3)
+if(process.env.CLIENT_EMAIL && process.env.PRIVATE_KEY && process.env.GA_VIEW_ID) {
+    dynamicPlugins.push({
+      resolve: `gatsby-plugin-guess-js`,
+      options: {
+        GAViewID: process.env.GA_VIEW_ID,
+        jwt: {
+          client_email: process.env.CLIENT_EMAIL,
+          private_key: process.env.PRIVATE_KEY,
+        },
+        period: {
+          startDate,
+          endDate: new Date(),
+        },
+      },
+    })
+}
+
 module.exports = {
   siteMetadata: {
     siteUrl: 'https://www.mikeyhilker.com',
@@ -113,5 +134,5 @@ module.exports = {
     //     },
     //   },
     // },
-  ],
+  ].concat(dynamicPlugins)
 }
